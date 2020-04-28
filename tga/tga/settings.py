9abf84fa746180b7a521c7846a18b437c6f9de93
vkhnychenko <khnychenko96@gmail.com>
2020-04-28 19:25:50 +0300

    update settings

--- tga/tga/settings.py
+++ tga/tga/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uj)b65p5!b6erfg0nd+#zklng#uj*s(roz*rs=qq24ocqx%5l&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                  '35.224.166.89']
